5bd1235dc23d8a0a2661fb766733df0a4e42cda1
Yan Gabriel <yangabrielogy@gmail.com>
2023-07-17 03:00:31 -0300

    🎉 init: shop-champion-select

---
+++ README.br.md
@@ -0,0 +1,40 @@
+<div align="center">
+
+# `🐧` Shop Champion Select <br>
+
+[![wakatime](https://wakatime.com/badge/user/89c5e1c8-9e67-43ef-bd0e-3ff9a4fde5e2/project/88cfd160-fb73-498c-9c64-8bd59b5869d1.svg)](https://wakatime.com/@programador/projects/ohrzmxpxdj)
+[![discord](https://img.shields.io/badge/Discord-%235865F2.svg?style=flat&logo=discord&logoColor=white&color=blue)](https://discordapp.com/users/854886148455399436) <br>
+![languages](https://img.shields.io/badge/Documentation-gray)
+[![english](https://img.shields.io/badge/-English-blue)](README.md)
+[![portuguese](https://img.shields.io/badge/-Português%20Brasileiro-blue)](README.br.md)
+
+Compre os campeões que precisa pela seleção de campeões! <br>
+Pesquise o campeão desejado, e clique no seu ícone para comprar :)
+
+</div>
+<br>
+
+## Dependências
+
+[Pengu Loader 1.0.4](https://github.com/PenguLoader/PenguLoader) (ou qualquer versão superior) <br>
+[controladoUtils.js](https://github.com/controlado/pengu-plugins/blob/master/controladoUtils.js) (deve estar na pasta de plugins)
+
+## Instalação
+
+1. Baixe os arquivos através das releases [download direto](https://github.com/controlado/shop-champion-select/releases/latest/download/shop-champion-select.zip)
+2. Extraia os arquivos para a pasta de plugins
+
+A estrutura deve ser algo parecida com isso:
+
+```
+📂 Pengu Loader
+└── 📂 plugins
+    ├── 📂 @default
+    ├── 📄 controladoUtils.js
+    └── 📂 shop-champion-select
+        ├── 📄 trans.json
+        ├── 📄 index.js
+        └── 📂 assets
+            └── 🎨 style.css
+
+```
---
+++ index.js
@@ -0,0 +1,131 @@
+import { layerManager, addRoutines, sleep, StoreBase, Champion, sendChatNotification, request } from "../controladoUtils";
+import trans from "./trans.json";
+import "./assets/style.css";
+
+/**
+ * @author balaclava
+ * @name shop-champion-select
+ * @link https://github.com/controlado/shop-champion-select
+ * @description Buy champions inside Champion Select! 🐧
+ */
+
+class Store extends StoreBase {
+    async getNotOwnedChampions() {
+        const response = await this.request("GET", "/storefront/v3/view/champions");
+        return response.data.catalog
+            .filter(champion => !champion.owned)
+            .map(champion => ({ ...champion, name: champion.name.toLowerCase() }));
+    }
+}
+
+class Icon {
+    constructor(store) {
+        this.element = document.createElement("div");
+        this.element.classList.add("mini-icon-champion");
+
+        this.img = document.createElement("img");
+        this.img.classList.add("mini-icon-img");
+        this.element.appendChild(this.img);
+
+        this.store = store;
+        this.champion = null;
+    }
+
+    setChampion(champion) {
+        this.champion = new Champion(champion.itemId, champion.ip);
+        this.img.src = `/lol-game-data/assets/v1/champion-icons/${this.champion.id}.png`;
+    }
+
+    show() {
+        this.element.style.display = "block";
+    }
+
+    hide() {
+        this.element.style.display = "none";
+    }
+}
+
+class Tooltip {
+    constructor(position) {
+        this.element = document.createElement("div");
+        this.element.setAttribute("id", "lol-uikit-tooltip-root");
+        this.element.classList.add("tooltip", "tooltip-random-champion");
+
+        const tooltipContent = document.createElement("lol-uikit-tooltip");
+        tooltipContent.setAttribute("data-tooltip-position", position);
+        tooltipContent.setAttribute("type", "system");
+
+        const tooltipBlock = document.createElement("lol-uikit-content-block");
+        tooltipBlock.setAttribute("type", "tooltip-system");
+
+        const tooltipText = document.createElement("p");
+        tooltipText.innerText = this.getLabel();
+
+        tooltipBlock.appendChild(tooltipText);
+        tooltipContent.appendChild(tooltipBlock);
+        this.element.appendChild(tooltipContent);
+    }
+
+    getLabel() {
+        const clientLocale = document.body.dataset.lang;
+        return trans.tooltip[clientLocale] || trans.tooltip[trans.missing];
+    }
+
+    show(receiver) {
+        const rect = receiver.getBoundingClientRect();
+        this.element.style.top = `${rect.top + rect.height - 38}px`;
+        this.element.style.left = `${rect.left + rect.width}px`;
+        layerManager.appendChild(this.element);
+    }
+
+    hide() {
+        layerManager.removeChild(this.element);
+    }
+}
+
+async function setupElements(selector) {
+    const gridHeader = document.querySelector(selector);
+    if (!gridHeader || gridHeader.hasAttribute("shop-champion-select")) { return; }
+
+    gridHeader.setAttribute("shop-champion-select", "true");
+
+    const store = new Store();
+    while (!store.token) {
+        await sleep(500);
+    }
+
+    const icon = new Icon(store);
+    gridHeader.appendChild(icon.element);
+
+    const tooltip = new Tooltip("right");
+    icon.element.addEventListener("mouseleave", () => tooltip.hide());
+    icon.element.addEventListener("mouseenter", () => tooltip.show(icon.element));
+    icon.element.addEventListener("click", async () => {
+        try { await store.buyChampions(icon.champion); }
+        catch (error) { sendChatNotification(error.response.data.message); }
+    });
+
+    const champions = await store.getNotOwnedChampions();
+    const championSearchInput = gridHeader.querySelector(".champion-input");
+    championSearchInput.addEventListener("input", () => {
+        if (!championSearchInput.value) {
+            icon.hide();
+            return;
+        }
+
+        const championSearchInputValue = championSearchInput.value.toLowerCase();
+        const filteredChampions = champions.filter(champion => champion.name.startsWith(championSearchInputValue));
+
+        if (filteredChampions.length === 1) {
+            icon.setChampion(...filteredChampions);
+            icon.show();
+        } else {
+            icon.hide();
+        }
+    });
+}
+
+addEventListener("load", () => {
+    addRoutines(() => setupElements(".champion-grid-header"));
+    console.debug("shop-champion-select: Report bugs to Balaclava#1912");
+});
